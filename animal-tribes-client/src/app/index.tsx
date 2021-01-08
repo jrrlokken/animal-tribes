e5e95ed70e15433887ec6145f35ac73536a44431
Joshua Lokken <jrrlokken@gmail.com>
2021-01-08 14:56:50 -0600

    Add frontend skeleton

---
+++ animal-tribes-client/src/app/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
+import Routes from 'src/routes'
+import config from 'src/config'
+
+const client = new ApolloClient({
+  uri: config.baseUrl,
+  request: async (operation) => {
+    const token = window.localStorage.getItem('token')
+    operation.setContext({
+      headers: {
+        Authorization: token || '',
+      },
+    })
+  },
+})
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Routes />
+    </ApolloProvider>
+  );
+}
+
+export default App;
